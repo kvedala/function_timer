5112b0f71f4d2f1048a0805bb9812bf35671b59d
Krishna Vedala <krishna.vedala@ieee.org>
2020-04-06 18:32:02 -0400

    library documentation + functionality

--- include/function_timer.h
+++ include/function_timer.h
@@ -1,21 +1,39 @@
+/**
+ * @file function_timer.h
+ * @author Krishna Vedala
+ * @brief Provides wrappers around C++ high-precesion timer from "std::chrono"
+ *  to C and C++ as a library. The duration is made available in double point
+ *  precision.
+ **/
 #ifndef __FUNCTION_TIMER_H
 #define __FUNCTION_TIMER_H
 
 #ifdef __cplusplus
 #include <chrono>
 #include <ctime>
+#include <cstdarg>
 
 using namespace std::chrono;
 
+/** @addtogroup CPP
+ * declare class structure for convenience
+ **/
 class function_timer
 {
 public:
-    function_timer(void (*function)(...));
-    function_timer(char *fn_name, void (*function)(...));
-    double run();
+    function_timer(){}; /**< create a new instance */
+
+    /** start timer measurement */
+    high_resolution_clock::time_point start_timer();
+
+    /** end timer measurement */
+    duration<double> end_timer();
+
+    /** convenience to get timer duration */
+    double get_duration();
 
 private:
-    char *_fn_name = 0;
+    // char *_fn_name = 0;
     high_resolution_clock::time_point _start_time;
     high_resolution_clock::time_point _end_time;
     duration<double> _duration;
@@ -23,9 +41,37 @@ private:
 
 extern "C"
 {
+#else // __cplusplus
+#include <stdarg.h>
+typedef struct function_timer function_timer;
 #endif // __cplusplus
 
-    // double function_timer(void (*function)(...));
+    /** @addtogroup C
+     * function to create a new timer instance to use in C programs
+     * @note The instantiated timer need to be explicitly deleted!
+     **/
+    extern function_timer *new_timer(void);
+
+    /** @addtogroup C
+     * function to start the timer in C programs.
+     * Requires timer to be created using the function `new_timer`
+     **/
+    extern void start_timer(function_timer *timer);
+
+    /** @addtogroup C
+     * function to end the timer in C programs.
+     * Requires timer to be created using the function `new_timer`. If the
+     * function `start_timer` was not called before, the duration will be
+     * inavlid.
+     **/
+    extern double end_timer(function_timer *timer);
+
+    /** @addtogroup C
+     * function to delete a timer instance
+     * @note The timer should be instantiated using the function `new_timer`
+     **/
+    extern void delete_timer(function_timer *timer);
+    // extern double time_function(void (*function)(...));
 
 #ifdef __cplusplus
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,55 @@
-#include <cstdarg>
+#include <cstdio>
 #include "function_timer.h"
 
-function_timer::function_timer(void (*function)(...))
+high_resolution_clock::time_point function_timer::start_timer()
 {
+    this->_start_time = high_resolution_clock::now();
+    return this->_start_time;
 }
 
-function_timer::function_timer(char *fn_name, void (*function)(...))
+duration<double> function_timer::end_timer()
 {
-    this->_fn_name = fn_name;
+    this->_end_time = high_resolution_clock::now();
+    this->_duration = duration_cast<duration<double>>(this->_end_time - this->_start_time);
+
+    return this->_duration;
 }
+
+double function_timer::get_duration()
+{
+    return this->_duration.count();
+}
+
+void start_timer(function_timer *timer)
+{
+    timer->start_timer();
+    return;
+}
+
+function_timer *new_timer()
+{
+    function_timer *timer = new function_timer();
+    if (!timer)
+    {
+        perror("Unable to create the timer!\n");
+        return 0;
+    }
+    return timer;
+}
+
+double end_timer(function_timer *timer)
+{
+    timer->end_timer();
+    return timer->get_duration();
+}
+
+void delete_timer(function_timer *timer)
+{
+    if (timer)
+        delete timer;
+}
+
+// double time_function(void (*function)(args...))
+// {
+//     function(args);
+// }
